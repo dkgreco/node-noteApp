303bf297d890086836b9aaf67c27521faa7dd483
Dustin K Greco <dgreco.co@gmail.com>
2019-07-11 14:01:16 -0600

    Project init with package.json and package-lock

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const fs = require('fs'),
-    getNotes = require('./utils/notes');
+    getNotes = require('./utils/notes'),
+    chalk = require('chalk');
 
-console.log(getNotes());
+const error = chalk.bold.red.underline,
+    success = chalk.bold.greenBright.underline.inverse;
+
+console.log(error('Error: Broken Chain Upstream...'));
+console.log(success('Successfully integrated chalk!!'));
+
 //fs.writeFileSync('notes.txt', 'Text to place into the file.');
 //fs.appendFileSync('notes.txt', 'New Material appended yo.');
