2ec9cb7e3fa08448bf5e5977c3351cf45d043f8b
Dustin K Greco <dgreco.co@gmail.com>
2019-07-10 18:12:03 -0600

    Challenge Completed

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const fs = require('fs');
+const fs = require('fs'),
+    getNotes = require('./utils/notes');
 
+console.log(getNotes());
 //fs.writeFileSync('notes.txt', 'Text to place into the file.');
-fs.appendFileSync('notes.txt', 'New Material appended yo.');
+//fs.appendFileSync('notes.txt', 'New Material appended yo.');
